56a10856e228cbc8a3420a5f22af489ada9beb61
gurpreet singh <gurpreetsingh2@telus.com>
2021-03-02 00:48:49 +0530

    Updates

---
+++ precache-manifest.101681bd0ffbd79a16772e3f28da1726.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996aa44a44279f8e5c376d6046d0121c",
+    "url": "/07mar2021/index.html"
+  },
+  {
+    "revision": "0f164f739aadf2ebc661",
+    "url": "/07mar2021/static/js/2.d7837064.chunk.js"
+  },
+  {
+    "revision": "bf00e45d78643d0d02c9",
+    "url": "/07mar2021/static/js/main.4f2134c9.chunk.js"
+  },
+  {
+    "revision": "4472210012ae6263c05a",
+    "url": "/07mar2021/static/js/runtime-main.4c08d244.js"
+  }
+]);
